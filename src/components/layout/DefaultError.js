14efe38fd15fabed24a5bc536f3072fa88013367
Tomasz Jarkowski <tomaszjarkowski1998@gmail.com>
2020-11-03 21:31:10 +0100

    create views components like loading, errors, empty books list

---
+++ src/components/layout/DefaultError.js
@@ -0,0 +1,9 @@
+const DefaultError = () => {
+  return (
+    <div className="error">
+      Sorry something went wrong. Please try again later ‼
+    </div>
+  );
+};
+
+export default DefaultError;
---
+++ src/components/layout/EmptyBooksList.js
@@ -0,0 +1,12 @@
+const EmptyBooksList = () => {
+  return (
+    <div className="empty">
+      Uuuups! Nothing found
+      <span role="img" aria-label="sad">
+        😓
+      </span>
+    </div>
+  );
+};
+
+export default EmptyBooksList;
